6d7e5bf325bfc159a39187545bad4bf141a61fae
Joseph <kartingman25@gmail.com>
2018-08-26 23:40:03 -0400

    Point Compatibility

--- TurretTest/src/com/joseph/gametemplate/math/DPoint.java
+++ TurretTest/src/com/joseph/gametemplate/math/DPoint.java
@@ -1,8 +1,10 @@
 package com.joseph.gametemplate.math;
 
+import java.awt.geom.Point2D;
+
 import com.joseph.gametemplate.math.physics.Vector;
 
-public class DPoint {
+public class DPoint extends Point2D {
 	private double x;
 	private double y;
 	
@@ -34,5 +36,11 @@ public class DPoint {
 	@Override
 	public String toString() {
 		return super.toString() + " x:" + this.x + " y:" + this.y;
+	}
+
+	@Override
+	public void setLocation(double x, double y) {
+		this.x = x;
+		this.y = y;
 	}
 }
